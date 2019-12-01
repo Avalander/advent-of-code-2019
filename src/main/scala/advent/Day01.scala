eb1cae6f9873c1c28cb7d498af55442f46b560c3
Avalander <darth.quesorico@gmail.com>
2019-12-01 10:10:24 +0100

    Solved problem 2 for day 1.

--- src/main/scala/advent/Day01.scala
+++ src/main/scala/advent/Day01.scala
@@ -1,17 +1,39 @@
 package advent
 
 import scala.io.Source
+import scala.annotation.tailrec
 
 object Fuel {
   def amountRequired (mass: Int): Int =
     mass / 3 - 2 
   
+  def fuelAdjusted (mass: Int): Int =
+    withFuel(mass)
+
+  @tailrec
+  private def withFuel (mass: Int, fuel: List[Int] = List()): Int = {
+    fuel match {
+      case Nil       => withFuel(mass, List(amountRequired(mass)))
+      case (x :: xs) => {
+        val fuelForLast = amountRequired(x)
+        if (fuelForLast <= 0) fuel.sum
+        else withFuel(mass, fuelForLast :: fuel)
+      }
+    }
+  }
+  
   def main (args: Array[String]): Unit = {
-    val input = Source.fromResource("day-01.txt").getLines()
+    val input = Source.fromResource("day-01.txt").getLines().toList
     val result = (for {
       line <- input
       value = line.toInt
     } yield amountRequired(value)).sum
-    println(result)
+    println(s"Problem 1: $result")
+
+    val result2 = (for {
+      line <- input
+      value = fuelAdjusted(line.toInt)
+    } yield value).sum
+    println(s"Problem 2: $result2")
   }
 }
