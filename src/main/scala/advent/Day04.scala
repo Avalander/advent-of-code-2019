0a2ab5f22589d3301ec30405c1b17ed7eafad628
Avalander <darth.quesorico@gmail.com>
2019-12-04 08:55:45 +0100

    Solved problem 2 for day 4.

--- src/main/scala/advent/Day04.scala
+++ src/main/scala/advent/Day04.scala
@@ -15,9 +15,27 @@ object Day04 {
       num = digits.mkString.toInt
       if num <= to && num >= from
     } yield num
-  
+
+  def validPasswords2 (from: Int, to: Int): Seq[Int] =
+    for {
+      a <- 2 to 9
+      b <- a to 9
+      c <- b to 9
+      d <- c to 9
+      e <- d to 9
+      f <- e to 9
+      digits = List(a, b, c, d, e, f)
+      groups = digits groupBy (x => x)
+      if groups.values exists (_.size == 2)
+      num = digits.mkString.toInt
+      if num <= to && num >= from
+    } yield num
+
   def main (args: Array[String]): Unit = {
     val result1 = validPasswords(278384, 824795).size
     println(s"Problem 1: $result1")
+
+    val result2 = validPasswords2(278384, 824795).size
+    println(s"Problem 2: $result2")
   }
 }
