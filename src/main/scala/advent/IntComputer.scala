840a0ab9430937ecacc4054cfee18f02b7bd4d8d
Avalander <darth.quesorico@gmail.com>
2019-12-05 10:10:48 +0100

    Solved day 5 problem 1.

--- src/main/scala/advent/IntComputer.scala
+++ src/main/scala/advent/IntComputer.scala
@@ -17,42 +17,48 @@ object IntComputer {
     val instruction = parseInstruction(code, program, index)
     instruction match {
       case Instruction(99, _) => (program, outputs)
-      case Instruction(1, List(a, b, dest)) => {
-        val value = a + b
-        println(s"$a, $b, $dest")
+      case Instruction(1, List(a, b, _)) => {
+        val valueA = getValue(index + 1, a, program)
+        val valueB = getValue(index + 2, b, program)
+        val dest = program(index + 3)
+        val value = valueA + valueB
         val result = program.patch(dest, List(value), 1)
         runExtended(result, index + 4, inputs, outputs)
       }
-      case Instruction(2, List(a, b, dest)) => {
-        val value = a * b
+      case Instruction(2, List(a, b, _)) => {
+        val valueA = getValue(index + 1, a, program)
+        val valueB = getValue(index + 2, b, program)
+        val dest = program(index + 3)
+        val value = valueA * valueB
         val result = program.patch(dest, List(value), 1)
         runExtended(result, index + 4, inputs, outputs)
       }
-      case Instruction(3, (dest :: _)) => {
+      case Instruction(3, _) => {
         val (input :: restInputs) = inputs
+        val dest = program(index + 1)
         val result = program.patch(dest, List(input), 1)
         runExtended(result, index + 2, restInputs, outputs)
       }
-      case Instruction(4, (dest :: _)) => {
-        val output = program(dest)
+      case Instruction(4, _) => {
+        val output = program(program(index + 1))
         runExtended(program, index + 2, inputs, outputs :+ output)
       }
     }
   }
 
+  private def getValue (index: Int, mode: Int, program: Program): Int =
+    mode match {
+      case 0 => program(program(index))
+      case 1 => program(index)
+      case _ => throw new MatchError(s"No param mode $mode")
+    }
+
   private def parseInstruction (code: Int, program: Program, index: Int) = {
-    val strCode = 
-      if (code < 10) defaultModes(code)
-      else f"$code%05d"
+    val strCode = f"$code%05d"
     val (a :: b :: c :: op) = strCode.toList
     val opCode = op.mkString.toInt
-    println(strCode)
     var argsLength = argsForInstruction(opCode, index)
-    val args = List(a, b, c).reverse take (argsLength) map (_.toString.toInt) zip (1 to 3) map {
-      case (0, i) => program(program(index + i))
-      case (1, i) => program(index + i)
-      case (x, _) => throw new MatchError(s"No param mode $x")
-    }
+    val args = List(a, b, c).reverse take (argsLength) map (_.toString.toInt)
     Instruction(opCode, args)
   }
 
@@ -64,13 +70,5 @@ object IntComputer {
       case 4 => 1
       case 99 => 0
       case x => throw new MatchError(s"Code $code at index $i")
-    }
-  
-  private def defaultModes (code: Int): String =
-    code match {
-      case 1 => "10001"
-      case 2 => "10002"
-      case 3 => "00103"
-      case 4 => "00104"
     }
 }
--- src/test/scala/advent/IntComputerTest.scala
+++ src/test/scala/advent/IntComputerTest.scala
@@ -23,10 +23,64 @@ class IntComputerTest extends FunSuite {
   }
 
   test("Should evaluate input and addition") {
-    val program = Vector(3,11,1,11,6,6,1100,1,1,12,99,0,0)
+    val program = Vector(
+      3,11,
+      1,11,6,6,
+      1100,1,1,11,
+      99,
+      0,0)
     val inputs = List(1)
     val (result, _) = runExtended(program, inputs = inputs)
-    assert(result == Vector(3,11,1,11,6,6,1101,1,1,12,99,1,4))
-    
+    assert(result == Vector(3,11,1,11,6,6,1101,1,1,11,99,2,0))
+  }
+
+  test("Addition") {
+    val cases = List(
+      (Vector(1,0,1,1,
+              99), Vector(1,1,1,1,99)),
+      (Vector(101, 5, 6, 6,
+              99, 40, 50),
+       Vector(101, 5, 6, 6,
+              99, 40, 55)),
+      (Vector(1001, 5, 6, 6,
+              99, 40, 50),
+       Vector(1001, 5, 6, 6,
+              99, 40, 46)),
+      (Vector(1101, 5, 6, 6,
+              99, 40, 50),
+       Vector(1101, 5, 6, 6,
+              99, 40, 11))
+    )
+
+    cases foreach {
+      case(input, output) =>
+        assert(runExtended(input)._1 == output)
+    }
+  }
+
+  test("Product") {
+    val cases = List(
+      (Vector(2,5,6,6,
+              99, 40, 50),
+       Vector(2,5,6,6,
+              99, 40, 2000)),
+      (Vector(102, 5, 6, 6,
+              99, 40, 50),
+       Vector(102, 5, 6, 6,
+              99, 40, 250)),
+      (Vector(1002, 5, 6, 6,
+              99, 40, 50),
+       Vector(1002, 5, 6, 6,
+              99, 40, 240)),
+      (Vector(1102, 5, 6, 6,
+              99, 40, 50),
+       Vector(1102, 5, 6, 6,
+              99, 40, 30))
+    )
+
+    cases foreach {
+      case(input, output) =>
+        assert(runExtended(input)._1 == output)
+    }
   }
 }
